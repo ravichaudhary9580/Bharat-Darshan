f13b9604daeccaf9caad947bfeeb4562e959366f
Ravi Chaudhary <ravichaudhary9580@outlook.com>
2025-11-09 01:53:30 +0530

    updated

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-// Minimal service worker - no caching, just makes PWA installable
-self.addEventListener('install', () => {
+const CACHE = 'bharat-darshan-v1';
+const assets = ['/', '/index.html', '/exploretrip.html', '/dashboard.html', '/signin.html', '/index.js', '/exploretrip.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(assets)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', () => {
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
