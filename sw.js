016efaa616533e703922b3ab83ef1b8f16541e28
Ravi Chaudhary <ravichaudhary9580@outlook.com>
2025-11-09 02:23:17 +0530

    push

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'bharat-darshan-v1';
-const assets = ['/', '/index.html', '/exploretrip.html', '/dashboard.html', '/signin.html', '/index.js', '/exploretrip.js'];
+const assets = ['/', '/index.html', '/exploretrip.html', '/dashboard.html', '/signin.html', '/privacy-policy.html', '/offline.html', '/index.js', '/exploretrip.js', '/image/home/logo.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(assets)));
@@ -12,5 +12,14 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(fetchRes => {
+      if (fetchRes.ok && e.request.url.startsWith(self.location.origin)) {
+        const clone = fetchRes.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+      }
+      return fetchRes;
+    }).catch(() => caches.match('/offline.html')))
+  );
 });
